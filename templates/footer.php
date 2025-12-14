cbba29f4964d58214f68b9c1547212bf0e84590b
EvgeniyKrastev <mostwanted82896@gmail.com>
2025-12-14 19:08:17 +0200

    ok

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
     </div>
-    <footer class="p-4 d-flex justify-content-center bg-primary-subtle text-primary-emphasis">
+    <footer class="p-4 d-flex justify-content-center bg-secondary text-primary-emphasis">
         <h2>Footer</h2>
     </footer>
 </body>
